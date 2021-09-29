31eb24748a31ab494d46bb211110a69265b60833
yongsoo97 <qjsro3975@naver.com>
2021-09-29 18:56:33 +0900

    [add] @Configuration 및 @Component

---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,15 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+    @Test
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        System.out.println(memberService);
+    }
+}
