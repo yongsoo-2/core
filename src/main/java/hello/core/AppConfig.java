b9f888bcf869f9578d3d51d76b6c582415fc3b3f
lee yong soo <ys.lee@twolinecode.com>
2021-12-13 14:33:56 +0900

    Revert "[add] @Configuration 및 @Component"
    This reverts commit 31eb24748a31ab494d46bb211110a69265b60833.
    
    # Conflicts:
    #	src/main/java/hello/core/AutoAppConfig.java
    #	src/test/java/hello/core/scan/AutoAppConfigTest.java

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,25 +15,21 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Bean
     public MemberService memberService() {
-        System.out.println("call AppConfig memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        System.out.println("call AppConfig memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
-        System.out.println("call AppConfig orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy() {
-        System.out.println("call AppConfig discountPolicy");
         return new RateDiscountPolicy();
     }
 
